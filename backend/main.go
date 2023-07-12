68d5589426c8f3b6ebcd9c3f2922d11a2b3bdae2
Yurii Palamarchuk <yurapalamarchuk111@gmail.com>
2023-07-12 16:35:22 +0200

    Refactor all handlers to server package from main

--- backend/main.go
+++ backend/main.go
@@ -1,76 +1,13 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"food_delivery/config"
-	"food_delivery/handler"
-	"food_delivery/middleware"
-	"food_delivery/repository"
-	"food_delivery/utils"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
+	"food_delivery/server"
 	_ "github.com/lib/pq"
-	"log"
-	"net/http"
 )
 
 func main() {
-	connStr := "postgres://food_delivery:password@localhost:5432/food_delivery?sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
 	cfg := config.NewConfig()
 
-	r := mux.NewRouter()
-	r.Use(utils.SendCfgToMiddleware(cfg))
-
-	customerRepository := repository.NewCustomerRepository(db)
-	customerHandler := handler.NewCustomerHandler(customerRepository, cfg)
-	customerRouter := r.PathPrefix("/customer").Subrouter()
-	customerRouter.Use(middleware.ValidateAccessToken)
-	customerRouter.HandleFunc("", customerHandler.GetCustomer).Methods(http.MethodGet)
-	customerRouter.HandleFunc("/{id}", customerHandler.UpdateCustomer).Methods(http.MethodPut)
-	customerRouter.HandleFunc("/{id}/change-password", customerHandler.UpdateCustomerPassword).Methods(http.MethodPut)
-	customerRouter.HandleFunc("/{id}", customerHandler.DeleteCustomer).Methods(http.MethodDelete)
-
-	authHandler := handler.NewAuthHandler(customerRepository, cfg)
-	r.HandleFunc("/register", authHandler.Register).Methods(http.MethodPost)
-	r.HandleFunc("/login", authHandler.Login).Methods(http.MethodPost)
-	refTokenRouter := r.PathPrefix("/refresh").Subrouter()
-	refTokenRouter.HandleFunc("", authHandler.Refresh).Methods(http.MethodGet)
-	refTokenRouter.Use(middleware.ValidateRefreshToken)
-
-	supplierRepository := repository.NewSupplierRepository(db)
-	supplierHandler := handler.NewSupplierHandler(supplierRepository)
-	r.HandleFunc("/supplier/{id}", supplierHandler.GetSupplierByID).Methods(http.MethodGet)
-	r.HandleFunc("/suppliers", supplierHandler.GetSuppliersByCategoryID).Queries("category_id", "{category_id}").Methods(http.MethodGet)
-	r.HandleFunc("/suppliers", supplierHandler.GetAllSuppliers).Methods(http.MethodGet)
-
-	categoryRepository := repository.NewCategoryRepository(db)
-	categoryHandler := handler.NewCategoryHandler(categoryRepository)
-	r.HandleFunc("/category/{id}", categoryHandler.GetCategoryByID).Methods(http.MethodGet)
-	r.HandleFunc("/categories", categoryHandler.GetCategoriesBySupplierID).Queries("supplier_id", "{supplier_id}").Methods(http.MethodGet)
-	r.HandleFunc("/categories", categoryHandler.GetAllCategories).Methods(http.MethodGet)
-
-	productRepository := repository.NewProductRepository(db)
-	productHandler := handler.NewProductHandler(productRepository)
-	r.HandleFunc("/product/{id}", productHandler.GetProductByID).Methods(http.MethodGet)
-	r.HandleFunc("/products", productHandler.GetAllProductsBySupplierIDAndCategoryID).Queries("supplier_id", "{supplier_id}", "category_id", "{category_id}").Methods(http.MethodGet)
-	r.HandleFunc("/products", productHandler.GetAllProductsByCategoryID).Queries("category_id", "{category_id}").Methods(http.MethodGet)
-	r.HandleFunc("/products", productHandler.GetAllProductsBySupplierID).Queries("supplier_id", "{supplier_id}").Methods(http.MethodGet)
-	r.HandleFunc("/products", productHandler.GetAllProducts).Methods(http.MethodGet)
-
-	orderRepository := repository.NewOrderRepository(db)
-	orderHandler := handler.NewOrderHandler(orderRepository, cfg)
-	ordersRouter := r.PathPrefix("/orders").Subrouter()
-	ordersRouter.Use(middleware.ValidateAccessToken)
-	ordersRouter.HandleFunc("", orderHandler.GetOrders).Methods(http.MethodGet)
-	ordersRouter.HandleFunc("", orderHandler.CreateOrder).Methods(http.MethodPost)
-
-	fmt.Println("Server is started...")
-	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
+	server.Start(cfg)
 }
